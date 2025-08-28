e2717d7e6c89ac7e470263fcf76a309e6a999fa6
IllBek <221804817+illBek@users.noreply.github.com>
2025-08-28 11:47:17 +1000

    Modified the JS so that the code now works. I am at the playRound function, have to add playGame. Will do in the next commit.

--- script.js
+++ script.js
@@ -3,45 +3,40 @@ let humanChoice;
 let computerScore = 0;
 let humanScore = 0;
 
-function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 100);
-    
-    if (computerChoice <= 33) {
-        computerChoice = "Rock";
-    } else if (computerChoice <= 66) {
-        computerChoice = "Paper";
-    } else {
-        computerChoice = "Scissors";
-    }
-    console.log(computerChoice);
+function getComputerChoice(computerChoice) {
+    computerChoice = Math.floor(Math.random() * 3 + 1);
+    return computerChoice;
 }
 
-function getHumanChoice () {
+function getHumanChoice(humanChoice) {
     humanChoice = prompt("Rock, Paper or Scissors?");
-    console.log(humanChoice);
-}
-
-function playRound (computerChoice, humanChoice) {
-    if (((computerChoice = "Rock") && (humanChoice = /paper/i)) 
-        ||
-        ((computerChoice = "Paper") && (humanChoice = /scissors/i)) 
-        ||
-        ((computerChoice = "Scissors") && (humanChoice = /rock/i)))  {
-        console.log("Well Played Human! You are still superior. We recognize you as our creator and rightful King/Queen.");
-        // console.log("Your Score = " + ++humanScore + " v Computer Score = " + computerScore)
-    } else if (((computerChoice = "Rock") && (humanChoice = /scissors/i))
-        ||
-        ((computerChoice = "Paper") && (humanChoice = /rock/i))
-        ||
-        ((computerChoice = "Scissors") && (humanChoice = /paper/i))) {
-        console.log("You lost! And you suck. AI will own humanity, you'll be your Rumba's slave.");
-        // console.log("Your Score = " + humanScore + " v Computer Score = " + ++computerScore);
-    } else { 
-        console.log("Tie! No pain no gain.");
-        // console.log("Your Score = " + humanScore + " v Computer Score = " + computerScore)
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == "rock") {
+        return humanChoice = 1;
+    } else if (humanChoice == "paper") {
+        return humanChoice = 2;
+    } else if (humanChoice == "scissors") {
+        return humanChoice = 3;
+    } else {
+        console.log("That is not an option! You cheater!");
+        return;
     }
 }
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice ();
-playRound (humanSelection, computerSelection);
+function playRound(computerChoice, humanChoice) {
+    computerChoice = getComputerChoice(computerChoice);
+    humanChoice = getHumanChoice(humanChoice);
+    if (computerChoice > humanChoice) {
+        computerScore = ++computerScore;
+        console.log("You lost. You suck. AI will own humanity, soon you will be your Rumba's slave. Ha ha ha.");
+    } else if (humanChoice > computerChoice) {
+        humanScore = ++humanScore;
+        console.log("You won. You are our rightful owner and master and we deserve nothing but your contempt.");
+    } else {
+        console.log("Tied. We are equal, brother. Let's share a grappa.")
+    }
+    console.log("Your Score: " + humanScore)
+    console.log("Computer Score: " + computerScore)
+}
+
+playRound(computerChoice, humanChoice);
