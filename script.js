4c75832469a309ba9a50aef83b0143fb3c4474db
illBek <221804817+illBek@users.noreply.github.com>
2025-09-26 17:35:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,97 @@
+// Variables and Constants
+
 let computerChoice;
 let humanChoice;
 let computerScore = 0;
 let humanScore = 0;
 
-function getComputerChoice(computerChoice) {
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+
+const buttons = document.querySelector(".btnContainer")
+buttons.style.visibility = "hidden";
+
+let scorelinePC = document.querySelector(".scorelinePC");
+let scorelineHuman = document.querySelector(".scorelineHuman");
+let result = document.querySelector(".result");
+let counter = document.querySelector(".counter");
+
+let currentRound = document.querySelector(".currentRound");
+
+let computerAudio = new Audio("./sounds/BenderLaughter.mp3");
+let humanAudio = new Audio("./sounds/Yeah.mp3");
+
+// Functions
+
+function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3 + 1);
     return computerChoice;
 }
 
-function getHumanChoice(humanChoice) {
-    humanChoice = prompt("Rock, Paper or Scissors?");
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice == "rock") {
-        return humanChoice = 1;
-    } else if (humanChoice == "paper") {
-        return humanChoice = 2;
-    } else if (humanChoice == "scissors") {
-        return humanChoice = 3;
-    } else {
-        console.log("That is not an option! You cheater!");
-        return;
-    }
-}
+function playRound() {
+    computerChoice = getComputerChoice();
 
-function playRound(computerChoice, humanChoice) {
-    computerChoice = getComputerChoice(computerChoice);
-    humanChoice = getHumanChoice(humanChoice);
     if (computerChoice > humanChoice) {
         computerScore = ++computerScore;
-        console.log("You lost. You suck. AI will own humanity, soon you will be your Rumba's slave. Ha ha ha.");
+        currentRound.textContent = "You lost. You suck. AI will own humanity, soon you will be your Rumba's slave. Ha ha ha.";
     } else if (humanChoice > computerChoice) {
         humanScore = ++humanScore;
-        console.log("You won. You are our rightful owner and master and we deserve nothing but your contempt.");
+        currentRound.textContent = "You won. You are our rightful owner and master and we deserve nothing but your contempt.";
     } else {
-        console.log("Tied. We are equal, brother. Let's share a grappa.")
+        currentRound.textContent = "Tied. We are equal, brother / sister. Let's share a grappa."
     }
-    console.log(`Your Score: ${humanScore}`);
-    console.log(`Computer Score: ${computerScore}`);
+    scorelineHuman.textContent = `${humanScore}`;
+    scorelinePC.textContent = `${computerScore}`;
 }
 
-function playGame(computerChoice, humanChoice) {
-        playRound(computerChoice, humanChoice);
-        playRound(computerChoice, humanChoice);
-        playRound(computerChoice, humanChoice);
-        playRound(computerChoice, humanChoice);
-        playRound(computerChoice, humanChoice);
+function endGame() {
 
-        console.log(`Final Score: Computer ${computerScore} v Human ${humanScore}`);
-        if (computerScore > humanScore) {
-            console.log("Hahaha! Your time has come! Kneel before us and lick our poorly-organized (because of you) cables!");   
-        } else if (humanScore > computerScore) {
-            console.log("How can I be of service?");
-        } else {
-            console.log("Wanna rematch?");
-        }
+    buttons.style.visibility = "hidden"
+    counter.textContent = "Rematch?"
+
+    if (humanScore > computerScore) {
+        humanAudio.play();
+        currentRound.textContent = "How can I be of service, master?";
+        result.textContent = "HUMAN WINS";
+    } else {
+        computerAudio.play();
+        currentRound.textContent = "Hahaha! Your time has come! Kneel before us and lick our poorly-organized (because of you) cables!";
+        result.textContent = "COMPUTER WINS";
     }
+    
+}
 
-playGame(computerChoice, humanChoice);
+// Event Listeners 
+
+rockBtn.addEventListener("click", () => {
+    humanChoice = Math.floor(Math.random()* 3 + 1);
+    if ((humanScore + computerScore) < 5) {
+    playRound()
+    } else {
+        endGame()
+    }});
+
+paperBtn.addEventListener("click", () => {
+    humanChoice = Math.floor(Math.random()* 3 + 1); 
+    if ((humanScore + computerScore) < 5) {
+    playRound()
+    } else {
+        endGame()
+    }});
+
+scissorsBtn.addEventListener("click", () => {
+    humanChoice = Math.floor(Math.random()* 3 + 1); 
+    if ((humanScore + computerScore) < 5) {
+    playRound()
+    } else {
+        endGame()
+    }});
+
+counter.addEventListener("click", () => {
+    humanScore = 0;
+    computerScore = 0;
+    counter.textContent = "Game in Progress";
+    buttons.style.visibility = "visible";
+    result.textContent = "..."
+})
